57a1a534442fbf9a6f015704b96d2d0741103ef6
Santiago <srabey14@gmail.com>
2024-01-18 17:05:06 -0300

    se crea hoja de tarea.js

---
+++ js/tareja.js
@@ -0,0 +1,8 @@
+/*Crear funciones que realicen las sgtes tareas: 
+1- Mostrar una lista con los nombres completos en orden alfabético
+2- Crear tarjetas de presentación con los datos de los usuarios
+3- Agregar un usuario mas al final de la lista
+4- Actualizar el nombre de un usuario
+5- Eliminar un usuario en particular
+6- Realizar busqueda de usuario cuyo apellido coincida con el término a buscar
+*/
